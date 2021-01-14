bcfd66a86c4ea86517e4662242ffb9c9ae55e8df
Alexander Chacón <alexanderchacon@outlook.com>
2021-01-14 18:43:56 -0500

    added js img welcome

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var rotate = 13;
+
+var pageX = $(document).width();
+var pageY = $(document).height();
+var mouseY=0;
+var mouseX=0;
+
+ $(document).mousemove(function( event ) {
+    mouseX = event.pageY;
+    mouseY = event.pageX;
+    rotateY = -(pageY/2-mouseY)/pageY*rotate;
+    rotateX = (pageX/2-mouseX)/pageX*rotate;
+    $('#img_welcome').css({ 'transform' : 'translate(-50%, -50%) scale(1) perspective( 1960px ) rotateY('+rotateY+'deg) rotateX('+rotateX+'deg)'});
+  });

250f69e350b271abf08b936e101b3f9342851610
Alexander Chacón <alexanderchacon@outlook.com>
2021-01-14 20:07:55 -0500

    added my work and listing all post in a for

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,21 @@
-var rotate = 13;
+$(function(){
 
-var pageX = $(document).width();
-var pageY = $(document).height();
-var mouseY=0;
-var mouseX=0;
+    var rotate = 13;
 
- $(document).mousemove(function( event ) {
+    var pageX = $(document).width();
+    var pageY = $(document).height();
+    var mouseY=0;
+    var mouseX=0;
+
+    $(document).mousemove(function( event ) {
     mouseX = event.pageY;
     mouseY = event.pageX;
     rotateY = -(pageY/2-mouseY)/pageY*rotate;
     rotateX = (pageX/2-mouseX)/pageX*rotate;
     $('#img_welcome').css({ 'transform' : 'translate(-50%, -50%) scale(1) perspective( 1960px ) rotateY('+rotateY+'deg) rotateX('+rotateX+'deg)'});
-  });
+    });
+
+    
+
+  
+});

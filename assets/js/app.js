4f55db516fbfba6ef9b1228e98f819565799582f
Alexander Chacón <alexanderchacon@outlook.com>
2021-01-15 18:43:29 -0500

    added hover state to link

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,69 +56,25 @@ $(function(){
 
     });
 
-  
-});
+    /**** HOVER LINKS ****/
 
-/*
+    $('.work_link').hover(function() {
 
-jQuery(function($) {
-
-    var preloadImgs = [];
-    $(document).ready(function() {
-        $('#wrapper_work-ul li h5 a').each(function() {
-            var img = new Image();
-            img.src = $(this).data('url');
-            preloadImgs.push(img);
+        $(this).addClass('work_link-change').css({
+            'color': '#2C2C2C'
         });
-    });
 
-    var wrapper_work_ul = document.getElementById("wrapper_work-ul");
-    var work_link = wrapper_work_ul.getElementsByClassName("work_link");
-    var current = document.getElementsByClassName("work_link-active");
+		$('.work_link, .work_link-title').not(this).css({
+            'opacity': '0.3'
+        })
 
-    var background_image = document.querySelectorAll("[data-url]")
+	}, function (){
 
-    for (var i = 0; i < work_link.length; i++) {
+		$('.work_link, .work_link-title').removeClass('work_link-change').css({
+            'opacity': '1',
+            'color': '#cabca7'
+        })
 
-        work_link[0].classList.add("work_link-active");
-
-        work_link[i].addEventListener("mouseover", addClass);
-
-        function addClass() {
-            current[0].className = current[0].className.replace(" work_link-active", "");
-            this.className += " work_link-active";
-        }
-
-    }
-
-if($(window).innerWidth() <= 768) {
-
-} else {
-$('#wrapper_work-loaded').css('background-image', 'url("' + background_image[0].dataset['url'] + '")');
-}
-
-    var $container_work = $('#wrapper_work-loaded');
-
-    $('.work_link').hover(function(e) {
-
-        var image = new Image();
-        image.crossororigin = 'Anonymous';
-
-        image.onload = function() {
-            $container_work.css('background-image', 'url(' + e.target.dataset.url + ')');
-            $container_work.addClass('img_grayscale-0');
-        };
-
-        image.src = e.target.dataset.url;
-
-    }, function() {
-
-        $container_work.removeClass('img_grayscale-0');
-
-    });
-
-
+	});
 
 });
-
-*/
